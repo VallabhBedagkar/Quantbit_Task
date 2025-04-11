da0c1712d783eea703c3a3babc234f35c43c4d19
VallabhBedagkar <125640678+VallabhBedagkar@users.noreply.github.com>
2025-04-11 19:16:50 +0530

    Add files via upload
    Task 5 Group Anagrams

---
+++ Anagram.py
@@ -0,0 +1,13 @@
+def group_anagrams(words):
+    anagrams = {}
+    for word in words:
+        sorted_word = "".join(sorted(word))
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word)
+        else:
+            anagrams[sorted_word] = [word]
+    return list((anagrams).values())
+
+
+words = ["eat","tea","tan","ate","nat","bat"]
+print(group_anagrams(words))
